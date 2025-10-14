95f2dc5b9d3cc78139de4ffc1c0a44e73aeccb5b
sebasschavez <sebaschavezolea@gmail.com>
2025-10-14 15:06:28 -0600

    tercer commit

--- db.php
+++ db.php
@@ -1,13 +1,15 @@
 <?php
-$host = "gondola.proxy.rlwy.net";
+$host = "shortline.proxy.rlwy.net";
 $user = "root";
-$pass = "KugVhFVrKcwcBPzWxRJbmHKpKDUdexqK";
-$db   = "railway";
-$port = 27604;
+$password = "twmQfNBiPUmPsBohVEgEQOTIXCeUOjWc";
+$dbname = "railway";
+$port = 59604;
 
-$conn = new mysqli($host, $user, $pass, $db, $port);
+$conn = new mysqli($host, $user, $password, $dbname, $port);
 
 if ($conn->connect_error) {
-    die("❌ Error de conexión: " . $conn->connect_error);
+    die("Error de conexión: " . $conn->connect_error);
+} else {
+    echo "Conexión exitosa ✅";
 }
 ?>
